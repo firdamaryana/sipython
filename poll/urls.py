ac439cf79992ac06c391d14662d5da701ca3f240
firdamaryana <firda_maryana94@yahoo.com>
2017-03-21 15:12:25 +0700

    poll app

--- poll/urls.py
+++ poll/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^help_me/$', views.help_me, name='help_me'),
     url(r'^(?P<question_id>[0-9]+)/$', views.detail_question, name='detail'),
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    url(r'^vote/$', views.vote, name='vote'),
 ]
--- poll/views.py
+++ poll/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
-
-from .models import Question
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from .models import Question,Choice
 
 
 def index(request):
 	context = {}
-	questions_query= Questions.objects.all()
-	context ['question'] = questions_query
+	questions_query= Question.objects.all()
+	context ['questions'] = questions_query
 	return render(request, 'poll/index.html',context)
 
 def help_me(request):
@@ -18,4 +19,16 @@ def detail_question(request, question_id):
 	question_query= Question.objects.get(id=question_id)
 	context['question'] = question_query
 	return render(request, 'poll/detail_question.html', context)
+
+def vote(request):
+	context = {}
+	choice_id = request.POST['choice']
+	selected_choice = Choice.objects.get(id=int(choice_id))
+	selected_choice.votes +=1
+	selected_choice.save()
+	return HttpResponseRedirect(reverse('poll:results', args=(selected_choice.question.id,)))
+
+def results(request, question_id):
+	question = Question.objects.get(pk=question_id)
+	return render(request, 'poll/result.html', {'question': question})
 
